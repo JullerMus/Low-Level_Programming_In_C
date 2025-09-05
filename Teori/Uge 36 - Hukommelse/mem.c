7e54a145da8ecea60d9a250996dc7c5fce123cef
JullerMus <juliusclykkegaard@gmail.com>
2025-09-05 10:29:30 +0200

    Added mem.c

---
+++ Teori/Uge 36 - Hukommelse/mem.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void my_func(int m);
+
+int main(){
+    int a = 1234;
+    int b = 5678;
+
+    b = a;
+    printf("Value of a: %d\n", a);
+    printf("Address of a: %d\n", &a);
+
+    printf("Value of b: %d\n", b);
+    printf("Address of b: %d\n", &b);
+
+    my_func(b);
+
+    printf("\n--- efter my_func ---\n");
+    printf("Value of a: %d\n", a);
+    printf("Address of a: %d\n", &a);
+
+    printf("Value of b: %d\n", b);
+    printf("Address of b: %d\n", &b);
+
+
+    return 0;
+}
+
+void my_func(int m){
+    int a = 512;
+    m = 21;
+
+    printf("\nStart my_func():\n");
+    printf("Value of a: %d\n", a);
+    printf("Address of a: %d\n", &a);
+
+    printf("Value of m: %d\n", m);
+    printf("Address of m: %d\n", &m);
+
+    printf("Afslut my_func()\n");
+}
